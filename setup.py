90f6d2f0cafe221f46b61c4cb27be3ae9d73a030
Tom O'Neill <tomoneill19@gmail.com>
2019-10-25 14:34:23 +0100

    small changes to setup.py for new machines

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
 #!/bin/python3
-import os, subprocess, sys
+import os
+import subprocess
+import sys
 
 yes = "-y" in sys.argv
 
 
-def package_requirements():
-    pkglist = ["nodejs", "texlive-bin", "latex-mk", "ccls"]
-    subprocess.run(["pikaur", "-S"] + pkglist)
+def pikaur(packages: list):
+    if "--pkg" in sys.argv or "--packages" in sys.argv:
+        subprocess.run([
+            "pikaur",
+            "-S",
+        ] + packages)
 
 
 def coc_plugins():
-    pluglist = ["coc-vimtex", "coc-python", "coc-json", "coc-java", "coc-html", "coc-css", "coc-ccls"]
+    pluglist = ["coc-vimtex", "coc-python", "coc-json", "coc-java", "coc-html", "coc-css", "coc-ccls", "coc-snippets"]
     subprocess.run(["nvim", "+CocInstall " + " ".join(pluglist)])
 
 
-def link(filepath, relative_name, sudo=False):
+def link(relative_name, filepath, sudo=False):
+    filepath = os.path.expanduser(filepath)
     if sudo:
         subprocess.run(["sudo", "rm", "-rf", filepath])
         subprocess.run(["sudo", "ln", "-s", os.getcwd() + relative_name, filepath])
@@ -23,40 +29,28 @@ def link(filepath, relative_name, sudo=False):
         subprocess.run(["ln", "-s", os.getcwd() + relative_name, filepath])
 
 
-zshrc = os.path.expanduser("~/.zshrc")
-zsh_dir = os.path.expanduser("~/.zsh")
-initdotvim = os.path.expanduser("~/.config/nvim/init.vim")
-pycodestyle = os.path.expanduser("~/.config/pycodestyle")
-i3 = os.path.expanduser("~/.config/i3")
-polybar = os.path.expanduser("~/.config/polybar")
-pacman = "/etc/pacman.conf"
-coc_settings = os.path.expanduser("~/.config/nvim/coc-settings.json")
-pylintrc = os.path.expanduser("~/.pylintrc")
-
-if yes or ("n" not in input("Install package dependencies? (Y/n)")):
-    package_requirements()
-
 if yes or ("n" not in input("Install zsh configs? (Y/n) ").lower()):
-    link(zshrc, "/zshrc")
+    pikaur(["zsh", "zsh-syntax-highlighting", "zsh-autocomplete", "pkgfile"])
+    link("/zshrc", "~/.zshrc")
     print("Installed zshrc")
-    link(zsh_dir, "/zsh-plugins")
+    link("/zsh-plugins", "~/.zsh")
     print("Installed .zsh folder")
 
 if yes or ("n" not in input("Install nvim configs? (Y/n) ").lower()):
+    pikaur(["neovim-nightly", "vim-plug", "neovim-symlinks", "nodejs", "texlive-bin", "latex-mk", "ccls"])
     try:
         os.makedirs(os.path.expanduser("~/.config/nvim"))
     except OSError:
         pass
-    link(initdotvim, "/init.vim")
+    link("/init.vim", "~/.config/nvim/init.vim")
     print("Installed init.vim")
-    link(coc_settings, "/coc-settings.json")
+    link("/coc-settings.json", "~/.config/nvim/coc-settings.json")
     print("Installed coc-settings")
-    link(pylintrc, "/pylintrc")
+    link("/pylintrc", "~/.pylintrc")
     print("Installed pylintrc")
     coc_plugins()
     print("Installed coc.nvim plugins")
 
-
 if yes or ("n" not in input("Install pacman.conf? (Y/n) ").lower()):
-    link(pacman, "/pacman.conf", sudo=True)
+    link("/pacman.conf", "/etc/pacman.conf", sudo=True)
     print("Installed pacman config")
